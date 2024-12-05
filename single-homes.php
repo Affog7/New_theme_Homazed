d8ebea27f23a7cc58ee3c3a613cced63436f76fd
Affog7 <augustinaffognon2000@gmail.com>
2024-12-05 08:02:27 +0100

    fin

--- single-homes.php
+++ single-homes.php
@@ -284,134 +284,137 @@ $post_id = get_the_ID();
 <!-- todo_augustin -->
 <!-- Popup Structure with Multi-Step Form -->
 <div id="editPostPopup" class="popup" style="display: none;    background: #6c6c64b5;">
-   <div class="popup-content">
-   	  <div class="popup-header">
-         <h2>Home Post</h2>
-         <div class="popup-controls">
-			 <?php
-			 // Récupérer le statut actuel du post
-			 $current_status = get_post_status($post_id);
+	<div class=" popup-content">
+		<div class="body-popup">
+			<div class="popup-header">
+				<h2>Home Post</h2>
+				<div class="popup-controls">
+					<?php
+					// Récupérer le statut actuel du post
+					$current_status = get_post_status($post_id);
 
-			 // Définir un libellé pour chaque statut
-			 $status_labels = array(
-				 'publish'  => 'Active',
-				 'private'  => 'Private',
-				 'erase'    => 'Erase',
-			 );
+					// Définir un libellé pour chaque statut
+					$status_labels = array(
+						'publish'  => 'Active',
+						'private'  => 'Private',
+						'erase'    => 'Erase',
+					);
 
-			 // Vérifier si le statut actuel est dans le tableau, sinon par défaut 'Active'
-			 $current_label = isset($status_labels[$current_status]) ? $status_labels[$current_status] : 'Active';
-			 ?>
+					// Vérifier si le statut actuel est dans le tableau, sinon par défaut 'Active'
+					$current_label = isset($status_labels[$current_status]) ? $status_labels[$current_status] : 'Active';
+					?>
 
-			 <div class="dropdown">
-				 <!-- Affichage dynamique du statut actuel -->
-				 <span id="post-status" class="active-status" onclick="toggleDropdown()">
+					<div class="dropdown">
+						<!-- Affichage dynamique du statut actuel -->
+						<span id="post-status" class="active-status" onclick="toggleDropdown()">
         <?php echo esc_html($current_label); ?> ▼
     </span>
-				 <div id="status-options" class="dropdown-content">
-					 <a href="#" onclick="setStatus('publish', <?php echo $post_id; ?>)">Active</a>
-					 <a href="#" onclick="setStatus('private', <?php echo $post_id; ?>)">Inactive</a>
-					 <a href="#" onclick="setStatus('erase', <?php echo $post_id; ?>)">Erase</a>
-				 </div>
-			 </div>
+						<div id="status-options" class="dropdown-content">
+							<a href="#" onclick="setStatus('publish', <?php echo $post_id; ?>)">Active</a>
+							<a href="#" onclick="setStatus('private', <?php echo $post_id; ?>)">Inactive</a>
+							<a href="#" onclick="setStatus('erase', <?php echo $post_id; ?>)">Erase</a>
+						</div>
+					</div>
 
-<!--			 --><?php //get_template_part( 'components/btn', null,
-//				 array(
-//					 'label' => 'Update',
-//					 'href' => "$post_link",
-//					 'target' => "_self",
-//					 'skin'  => 'ghost',
-//					 'icon-only'  => false,
-//					 'disabled'  => false,
-//					 'icon-position' => '', // left or right
-//					 'icon' => '',
-//					 'additional-classes' => 'square',
-//					 'data-attribute' => '',
-//					 'theme' => "",
-//				 )
-//			 ); ?>
+					<!--			 --><?php //get_template_part( 'components/btn', null,
+					//				 array(
+					//					 'label' => 'Update',
+					//					 'href' => "$post_link",
+					//					 'target' => "_self",
+					//					 'skin'  => 'ghost',
+					//					 'icon-only'  => false,
+					//					 'disabled'  => false,
+					//					 'icon-position' => '', // left or right
+					//					 'icon' => '',
+					//					 'additional-classes' => 'square',
+					//					 'data-attribute' => '',
+					//					 'theme' => "",
+					//				 )
+					//			 ); ?>
 
-            <span class="close-btn-circle">&times;</span>
-         </div>
-      </div>
+					<span class="close-btn-circle">&times;</span>
+				</div>
+			</div>
 
-      <!-- Divider -->
-      <hr>
+			<!-- Divider -->
+			<hr>
 
-      <!-- Sommaire (Step 0) -->
-      <div class="form-step" id="step0">
-         <h3>Sommaire des étapes</h3>
-         <ul class="step-list">
-            <li><a href="#" onclick="goToStep(1)">Media</a></li>
-            <li><a href="#" onclick="goToStep(2)">Location</a></li>
-            <li><a href="#" onclick="goToStep(3)">Texts & Key Info</a></li>
-            <li><a href="#" onclick="goToStep(4)">Connections</a></li>
-            <li><a href="#" onclick="goToStep(5)">Event</a></li>
-            <li><a href="#" onclick="goToStep(6)">Premium</a></li>
-         </ul>
-      </div>
+			<!-- Sommaire (Step 0) -->
+			<div class="form-step" id="step0">
+				<h3>Sommaire des étapes</h3>
+				<ul class="step-list">
+					<li><a href="#" onclick="goToStep(1)">Media</a></li>
+					<li><a href="#" onclick="goToStep(2)">Location</a></li>
+					<li><a href="#" onclick="goToStep(3)">Texts & Key Info</a></li>
+					<li><a href="#" onclick="goToStep(4)">Connections</a></li>
+					<li><a href="#" onclick="goToStep(5)">Event</a></li>
+					<li><a href="#" onclick="goToStep(6)">Premium</a></li>
+				</ul>
+			</div>
 
-      <!-- Autres étapes du formulaire -->
-      <div class="form-step" id="step1" style="display:none;">
-         <h3>Media</h3>
-         <main class="modal__content contact__form contact__form--light">
-				<?php	echo do_shortcode('[gallery_manager  max_images="15" size="medium" allowed_extensions="jpg,png"  post_id="' . $post_id . '"]');
-				; ?>
+			<!-- Autres étapes du formulaire -->
+			<div class="form-step" id="step1" style="display:none;">
+				<h3>Media</h3>
+				<main class="modal__content contact__form contact__form--light">
+					<?php	echo do_shortcode('[gallery_manager  max_images="15" size="medium" allowed_extensions="jpg,png"  post_id="' . $post_id . '"]');
+					; ?>
 
-<!--				--><?php	//echo do_shortcode( '[gravityform id="4" title="false"  field_values="post_retrieved_id=' . $post_id . '"]' ); ?>
-		 </main>
-      </div>
-      <div class="form-step" id="step2" style="display:none;">
-         <h3>Location</h3>
-		  <main class="modal__content contact__form contact__form--light">
-			  <?php echo do_shortcode( '[gravityform id="17" title="false" ajax="true" field_values="post_retrieved_id=' . $post_id . '"]' ); ?>
-		  </main>
-      </div>
-      <div class="form-step" id="step3" style="display:none;">
-         <h3>Texts & Key Info</h3>
-		<main class="modal__content contact__form contact__form--light">
+					<!--				--><?php	//echo do_shortcode( '[gravityform id="4" title="false"  field_values="post_retrieved_id=' . $post_id . '"]' ); ?>
+				</main>
+			</div>
+			<div class="form-step" id="step2" style="display:none;">
+				<h3>Location</h3>
+				<main class="modal__content contact__form contact__form--light">
+					<?php echo do_shortcode( '[gravityform id="17" title="false" ajax="true" field_values="post_retrieved_id=' . $post_id . '"]' ); ?>
+				</main>
+			</div>
+			<div class="form-step" id="step3" style="display:none;">
+				<h3>Texts & Key Info</h3>
+				<main class="modal__content contact__form contact__form--light">
 
-<!--		--><?php //echo do_shortcode( '[gravityform id="3" ajax="true" title="false" field_values="post_retrieved_id=' . $post_id . '"]' ); ?>
-<!---->
-<!--		--><?php //echo do_shortcode( '[gravityform id="13" title="false" field_values="post_retrieved_id=' . $post_id . '"]' ); ?>
-<!---->
-<!--		--><?php //echo do_shortcode( '[gravityform id="14" title="false" field_values="post_retrieved_id=' . $post_id . '"]' ); ?>
-<!---->
-<!--		 --><?php //echo do_shortcode( '[gravityform id="7" title="false" field_values="post_retrieved_id=' . $post_id . '"]' ); ?>
+					<!--		--><?php //echo do_shortcode( '[gravityform id="3" ajax="true" title="false" field_values="post_retrieved_id=' . $post_id . '"]' ); ?>
+					<!---->
+					<!--		--><?php //echo do_shortcode( '[gravityform id="13" title="false" field_values="post_retrieved_id=' . $post_id . '"]' ); ?>
+					<!---->
+					<!--		--><?php //echo do_shortcode( '[gravityform id="14" title="false" field_values="post_retrieved_id=' . $post_id . '"]' ); ?>
+					<!---->
+					<!--		 --><?php //echo do_shortcode( '[gravityform id="7" title="false" field_values="post_retrieved_id=' . $post_id . '"]' ); ?>
 
-		 <?php echo do_shortcode( '[gravityform id="8" ajax="true" title="false" field_values="post_retrieved_id=' . $post_id . '"]' ); ?>
+					<?php echo do_shortcode( '[gravityform id="8" ajax="true" title="false" field_values="post_retrieved_id=' . $post_id . '"]' ); ?>
 
-		</main>
-      </div>
-      <div class="form-step" id="step4" style="display:none;">
-         <h3>Connections</h3>
-		 <main class="modal__content contact__form contact__form--light">
-				<?php echo do_shortcode( '[gravityform id="11" ajax="true" title="false" field_values="post_retrieved_id=' . $post_id . '"]' ); ?>
-		</main>      </div>
-      <div class="form-step" id="step5" style="display:none;">
-         <h3>Event</h3>
-         <main class="modal__content contact__form contact__form--light">
-				<?php echo do_shortcode( '[gravityform id="15" title="false" ajax="true" field_values="post_retrieved_id=' . $post_id . '"]' ); ?>
-		 </main>
-      </div>
-      <div class="form-step" id="step6" style="display:none;">
-         <h3>Premium</h3>
-		  <main class="modal__content contact__form contact__form--light">
-		  <?php echo do_shortcode( '[gravityform id="20" ajax="true" title="false" field_values="post_retrieved_id=' . $post_id . '"]' ); ?>
-		  </main>
-	  </div>
+				</main>
+			</div>
+			<div class="form-step" id="step4" style="display:none;">
+				<h3>Connections</h3>
+				<main class="modal__content contact__form contact__form--light">
+					<?php echo do_shortcode( '[gravityform id="11" ajax="true" title="false" field_values="post_retrieved_id=' . $post_id . '"]' ); ?>
+				</main>      </div>
+			<div class="form-step" id="step5" style="display:none;">
+				<h3>Event</h3>
+				<main class="modal__content contact__form contact__form--light">
+					<?php echo do_shortcode( '[gravityform id="15" title="false" ajax="true" field_values="post_retrieved_id=' . $post_id . '"]' ); ?>
+				</main>
+			</div>
+			<div class="form-step" id="step6" style="display:none;">
+				<h3>Premium</h3>
+				<main class="modal__content contact__form contact__form--light">
+					<?php echo do_shortcode( '[gravityform id="20" ajax="true" title="false" field_values="post_retrieved_id=' . $post_id . '"]' ); ?>
+				</main>
+			</div>
 
-      <!-- Contrôles de navigation -->
-      <div class="form-navigation">
-         <button id="prevBtn" onclick="navigateSteps(-1)">Previous</button>
-         <span id="step-count">0 / 6</span>
-         <button id="nextBtn" onclick="navigateSteps(1)">Next</button>
-      </div>
-		<div>
-			<p class="active-status" id="status_notif_"></p>
+			<!-- Contrôles de navigation -->
+			<div class="form-navigation">
+				<button id="prevBtn" onclick="navigateSteps(-1)">Previous</button>
+				<span id="step-count">0 / 6</span>
+				<button id="nextBtn" onclick="navigateSteps(1)">Next</button>
+			</div>
+			<div>
+				<p class="active-status" id="status_notif_"></p>
+			</div>
+
 		</div>
+	</div>
 
-   </div>
 </div>
 
 
